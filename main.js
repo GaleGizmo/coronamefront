7704d0a70bd675fd92058b81c16c7572a2829f5b
Miguel <miguelaf71@hotmail.com>
2024-02-23 23:15:27 +0100

    votaciones

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
+// import { Notyf } from 'notyf'
+
+const menuCoristas = document.getElementById("select_name");
+
+let coristaNameParsed = "";
+const notyf = new Notyf({
+  types: [
+    {
+      type: "hello",
+      background: "blue",
+      icon: false,
+      position: {
+        x: 'center',
+        y: 'top',
+      },
+    },
+  ],
+});
+
+
+function checkCorista() {
+  const coristaName = localStorage.getItem("coristaName");
+
+  if (coristaName) {
+    coristaNameParsed = JSON.parse(coristaName);
+    hideMenu();
+    notyf.open({
+      type: "hello",
+      message: "Ola, " + coristaNameParsed.name,
+    });
+  }
+}
+function hideMenu() {
+  menuCoristas.style.display = "none";
+}
 const getAllNames = async () => {
-  const data = await fetch(
-    "https://coronamebackend.app.rockthebarrio.es/nombres"
-  );
+  const data = await fetch("https://coronamebackend.app.rockthebarrio.es/nombres");
   const names = await data.json();
   if (typeof names != "string") {
     drawNames(names);
@@ -10,42 +43,147 @@ const getAllNames = async () => {
 const drawNames = (names) => {
   const namesList$$ = document.getElementById("show_names");
   namesList$$.innerHTML = "";
-  const sortedNames = names.sort((a, b) => a.name.localeCompare(b.name));
+
+  const sortedNames = names.sort((a, b) => b.points - a.points);
 
   for (const name of sortedNames) {
     const cardName$$ = document.createElement("div");
     const name$$ = document.createElement("h2");
     const corista$$ = document.createElement("h4");
     const by = document.createElement("p");
-    const motivo$$ = document.createElement("p");
+    const points = document.createElement("span");
+    // const motivo$$ = document.createElement("p");
     cardName$$.classList.add("mb-3");
     cardName$$.classList.add("card_name");
     by.classList.add("by");
+    points.classList.add("points");
+    points.textContent = name.points;
     name$$.textContent = name.name;
     corista$$.textContent = name.author.name;
-    motivo$$.textContent = name.reason;
+    if (coristaNameParsed && coristaNameParsed.namesVoted.includes(name._id)) {
+      cardName$$.classList.add("selected");
+    }
+    // motivo$$.textContent = name.reason;
     by.textContent = "by";
+
     cardName$$.appendChild(name$$);
     cardName$$.appendChild(by);
     cardName$$.appendChild(corista$$);
-    cardName$$.appendChild(motivo$$);
+    cardName$$.appendChild(points);
+    // cardName$$.appendChild(motivo$$);
 
     namesList$$.appendChild(cardName$$);
+    cardName$$.addEventListener("click", () => {
+      toggleName(name._id);
+      cardName$$.classList.toggle("selected");
+    });
   }
 };
+function toggleName(coroName) {
+  if (coristaNameParsed.namesVoted && coristaNameParsed.namesVoted.includes(coroName)) {
+    coristaNameParsed.namesVoted = coristaNameParsed.namesVoted.filter(
+      (name) => name != coroName
+    );
+  } else {
+    coristaNameParsed.namesVoted.push(coroName);
+  }
+}
+
 let coristasList = null;
+
 const getCoristas = async () => {
-  const data = await fetch(
-    "https://coronamebackend.app.rockthebarrio.es/coristas"
-  );
+  const data = await fetch("https://coronamebackend.app.rockthebarrio.es/coristas");
   const coristas = await data.json();
   if (typeof coristas != "string") {
     coristasList = coristas;
-    const coristasNames = coristas.map((el) => el.name);
+    const coristasNames = coristas
+      .filter((member) => member.name !== "Anónimo")
+      .map((el) => el.name);
 
     loadCoristas(coristasNames);
   }
 };
+const selectField$$ = document.getElementById("coristas");
+
+checkCorista();
+getCoristas();
+getAllNames();
+
+const loadCoristas = (members) => {
+  const sortedMembers = members.sort((a, b) => a.localeCompare(b));
+
+  for (const member of sortedMembers) {
+    const option = document.createElement("option");
+    option.value = member;
+    option.text = member;
+    selectField$$.appendChild(option);
+  }
+};
+
+const botonSetCorista$$ = document.getElementById("set_corista");
+const sendVotesButton$$ = document.getElementById("send_votes");
+
+function setCorista() {
+  hideMenu();
+  const coristaData = coristasList.filter(
+    (corista) => corista.name === selectField$$.value
+  );
+    notyf.open({
+      type: "hello",
+      message: "Ola, " + coristaData[0].name,
+    })
+  localStorage.setItem("coristaName", JSON.stringify(coristaData[0]));
+}
+
+const sendVotes = async () => {
+  if (coristaNameParsed.namesVoted.length === 0) {
+    notyf.error({
+      message: "Non marcaches ningún nome",
+      duration: 2000,
+      position: {
+        x: "center",
+        y: "top",
+      },
+    });
+    return;
+  }
+  try {
+    const dataToSend = {
+      votados: coristaNameParsed.namesVoted,
+      corista: coristaNameParsed._id,
+    };
+
+    coristaNameParsed.voted = true;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    };
+    const response = await fetch(
+      "https://coronamebackend.app.rockthebarrio.es/nombres/addVoto",
+      options
+    );
+    const responseData = await response.json();
+    notyf.success({
+      message: responseData.message,
+      duration: 2000,
+      position: {
+        x: "center",
+        y: "top",
+      },
+    });
+    localStorage.setItem("coristaName", JSON.stringify(coristaNameParsed));
+
+    await getAllNames();
+  } catch (err) {
+    console.error("Error en el envio:", err);
+  }
+};
+botonSetCorista$$.addEventListener("click", setCorista);
+sendVotesButton$$.addEventListener("click", sendVotes);
+
 // const loadCoristas = (members) => {
 //   const anonymousOption = "Anónimo";
 //   const sortedMembers = members
@@ -94,22 +232,19 @@ const getCoristas = async () => {
 //   showForm(itemsToHide)
 //   addNameIcon.classList.add("hidden")
 // };
-// getCoristas();
-getAllNames();
 
 // const nameDiv$$ = document.getElementById("name_div");
 // const coristaDiv$$ = document.getElementById("corista_div");
 // const reasonDiv$$ = document.getElementById("motivo_div");
 // const sendButton$$= document.getElementById("send_name")
 // const showForm = (itemsToChange) => {
-  
+
 //   for (const element of itemsToChange){
 //     element.classList.toggle("hidden")
 //     element.classList.toggle("shown")
 //   }
-  
-// };
 
+// };
 
 // addNameIcon.addEventListener("click", ()=>{
 //     const itemsToShow=document.querySelectorAll(".shown")
